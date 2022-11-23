cd6a6f42c19ab395af37864d1b649146f4cb9499
saigyou_ayakashi <minuit1993@live.fr>
2022-11-23 20:53:20 +0100

    test commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React(if)</h1>
       <div className="card">
         <button type="submit">count</button>
         <p>
