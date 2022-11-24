b24c772a4701d5b021280d8c1a34fc26d2f31be2
VICTOIREMICHELDLM <victoiremicheldlm@gmail.com>
2022-11-24 22:27:21 +0100

    fixed css and addded animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       </header>
       <div className="home-info">
         <span>
-          <h1> Envie de vous évadez ? </h1>
+          <h2> Envie de vous évadez ? </h2>
         </span>
         <span>
           {" "}
@@ -29,28 +29,61 @@ export default function Home() {
           </p>
         </span>
         <span>
+          <p>Il n’y a plus qu’à suivre notre programme.</p>
           <p>
-            Il n’y a plus qu’à suivre notre programme. Bonus : nos voyages
-            prévoient un critère écoresponsable pour vous permettre de partir la
-            conscience tranquille.
+            {" "}
+            Bonus : nos voyages prévoient un critère écoresponsable pour vous
+            permettre de partir la conscience tranquille.
           </p>
         </span>
       </div>
       <div className="home-start">
-        <h1>Demmarrer votre Journey</h1>
+        <h2>Demarrer votre Journey</h2>
         <button type="button" className="button-home">
           {" "}
           GO{" "}
         </button>
       </div>
-      <div className="bottom-home-page">
-        <img
-          src={bottomIcon}
-          alt="Welcome Illustration"
-          className="bottom-icon"
-        />
+      <div className="container-animation">
+        <div className="container-animation">
+          <div className="campfire-wrapper">
+            <div className="tree-container-back">
+              <div className="tree-8" />
+              <div className="tree-9" />
+              <div className="tree-10" />
+            </div>
+            <div className="rock-container">
+              <div className="rock-big" />
+              <div className="rock-small">
+                <div className="rock-1" />
+                <div className="rock-2" />
+                <div className="rock-3" />
+                <div className="rock-4" />
+              </div>
+            </div>
+            <div className="smoke-container">
+              <svg className="svg-tree">
+                <path d="M 150 0 Q 200 100 100 250 C 0 450 120 400 50 600  " />
+              </svg>
+              <div className="fire-container">
+                <div className="flame-1" />
+                <div className="flame-2" />
+                <div className="flame-3" />
+              </div>
+            </div>
+            <div className="tree-container-front">
+              <div className="tree-1" />
+              <div className="tree-2" />
+              <div className="tree-3" />
+              <div className="tree-4" />
+              <div className="tree-5" />
+              <div className="tree-6" />
+              <div className="tree-7" />
+            </div>
+          </div>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
