1e32a47374a2c04cc8ed318c11714891c1132ecc
mlgs <mathieu.langlois@voxm.live>
2022-11-24 15:45:03 +0100

    Fake data jour 1

---
+++ frontend/src/assets/data/data.js
@@ -0,0 +1,31 @@
+const jour1 = [
+{ id:"1",
+name:"Train Paris > Rome",
+address:"Gare de Lyon",
+description:"Pour partir à Rome, privilégiez le train qui est la moins consommatrice en CO2. En plus, en TGV, c'est seulement 4h30 de voyage",
+countCo2:"3,1",
+website:"https://www.sncf.com/"
+},
+{ id:"2",
+name:"Restaurant E Mozzarella",
+address:"Via del Piè di Marmo 32, 00186 Rome Italie",
+description:"Avec ses circuits courts et son choix de légumes de saison, les pizzas du restaurant E Mozzarella sont les plus healthy de Rome",
+countCo2:"1,5",
+website:"https://www.facebook.com/pages/Pizza%20e%20Mozzarella/177933742916115/"
+},
+{ id:"3",
+name:"Le Colisée",
+address:"piazza del colosseo, 00186 Rome Italie",
+description:"L'un des symboles les plus connus de Rome, l'amphithéàtre du Colisée reste comme le plus grand jamais contruit dans l'Histoire",
+countCo2:"0",
+website:"https://www.il-colosseo.it/fr/"
+},
+{ id:"4",
+name:"Starhotels Michelangelo",
+address:"Via della Stazione di S. Pietro, 14, 00165 Roma RM, Italy",
+description:"La totalité des éléments architecturaux et design font de Starhotels E.c.ho. un véritable joyau de la construction respectueuse de l’environnement",
+countCo2:"5",
+website:"https://www.starhotels.com/fr/nos-hotels/michelangelo-rome/"
+},]
+
+export default jour1;
