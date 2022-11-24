5170d4f22871e23d45c4646cd32302c1134f8024
saigyou_ayakashi <minuit1993@live.fr>
2022-11-24 14:01:39 +0100

    finish footer

--- frontend/src/components/layout-components/Footer/Footer.jsx
+++ frontend/src/components/layout-components/Footer/Footer.jsx
@@ -1,18 +1,25 @@
 import "./Footer.css";
+import twitter from "../../../assets/img/social/twitter.svg";
+import facebook from "../../../assets/img/social/facebook.svg";
+import instagram from "../../../assets/img/social/instagram.svg";
 
 export default function Footer() {
+  const twitterLink = "https://twitter.com/home";
+  const facebookLink = "https://www.facebook.com/";
+  const instagramLink = "https://www.instagram.com/";
+
   return (
     <footer>
       <p id="copyrigth">Envisioned journeys</p>
       <div className="container-social">
-        <a className="social" href="https://www.facebook.com/">
-          facebook
+        <a href={twitterLink} target="twitter icon social">
+          <img src={twitter} alt="twitter" className="social" />
         </a>
-        <a className="social" href="https://www.instagram.com/">
-          instagram
+        <a href={facebookLink} target="facebook icon social">
+          <img src={facebook} alt="twitter" className="social" />
         </a>
-        <a className="social" href="https://www.twitter.com/">
-          twitter
+        <a href={instagramLink} target="instagram icon social">
+          <img src={instagram} alt="twitter" className="social" />
         </a>
       </div>
     </footer>
