396c073102b08115707842e9553a2c511218be1c
mlgs <mathieu.langlois@voxm.live>
2022-11-24 19:38:01 +0100

    reopen function quiz on travel.jsx

--- frontend/src/pages/Travel.jsx
+++ frontend/src/pages/Travel.jsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/layout-components/Navbar/Navbar";
 import Footer from "../components/layout-components/Footer/Footer";
-import ResultItineraire from "../components/ResultItineraire";
+import Quiz from "../components/Quiz/Quiz";
 
 export default function Travel() {
   return (
     <div className="travel">
       <Navbar />
-      <ResultItineraire />
+      <Quiz />
       <Footer />
     </div>
   );
