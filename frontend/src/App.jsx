1b0ae4c77d963ba5c4be8da689178bb4bd3e68bc
saigyou_ayakashi <minuit1993@live.fr>
2022-11-24 11:58:00 +0100

    Finish navbar style and component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,9 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import Navbar from "./components/layout-components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React(if)</h1>
-      <div className="card">
-        <button type="submit">count</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
     </div>
   );
 }
