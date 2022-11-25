b71dababefa3cbce8b83d70a96f31b1c360f881d
LAURE JANIN <ljanin51@gmail.com>
2022-11-25 11:45:32 +0100

    fix css cards quiz

--- frontend/src/components/Quiz/Quiz.jsx
+++ frontend/src/components/Quiz/Quiz.jsx
@@ -2,7 +2,7 @@ import "./Quiz.css";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Quiz() {
   const [btnActive, setBtnActive] = useState("");
@@ -28,10 +28,11 @@ export default function Quiz() {
   const handleSubmit = () => {
     if (showChoiseDestination === false) {
       navigate("/toulouse");
+    } else if (destination === "Elmer") {
+      navigate("/elmer");
     } else {
-      if (destination == "Elmer") {
-        navigate("/elmer");
-      } else { navigate("/results") }}
+      navigate("/results");
+    }
   };
 
   const handleDestination = (e) => {
@@ -39,37 +40,40 @@ export default function Quiz() {
   };
 
   return (
-      <div className="content">
-        <section>
-          <h1 className="h1">
-            <span className="t-bold">imaginez</span> votre voyage{" "}
-            <span className="t-bold">dès maintenant</span>
-          </h1>
+    <div className="content">
+      <section>
+        <h1 className="h1">
+          <span className="t-bold">imaginez</span> votre voyage{" "}
+          <span className="t-bold">dès maintenant</span>
+        </h1>
+        <div className="cards">
           <div className="card">
             <div className="card-title mg-b-08">
               Connaissez-vous votre destination ?
             </div>
             <div className="form">
               <button
-                  id="choiseDestination"
-                  onClick={handleClickChoiseDestination}
-                  className={
-                    showChoiseDestination === false ? "btn-active button" : "button"
-                  }
-                  type="button"
+                id="choiseDestination"
+                onClick={handleClickChoiseDestination}
+                className={
+                  showChoiseDestination === false
+                    ? "btn-active button"
+                    : "button"
+                }
+                type="button"
               >
                 {showChoiseDestination === true ? "NON" : "OUI"}
               </button>
               {showChoiseDestination === true ? (
-                  <input
-                      className="w-70 mg-left-5"
-                      type="text"
-                      name="destination"
-                      placeholder="Entrez votre destination"
-                      onChange={(e) => setDestination(e.target.value)}
-                  />
+                <input
+                  className="w-70 mg-left-5"
+                  type="text"
+                  name="destination"
+                  placeholder="Entrez votre destination"
+                  onChange={(e) => setDestination(e.target.value)}
+                />
               ) : (
-                  false
+                false
               )}
             </div>
           </div>
@@ -80,17 +84,17 @@ export default function Quiz() {
             <div className="inline space-b mg-b-08">
               <div className="card-title mg-r-0-5">Départ: </div>
               <DatePicker
-                  dateFormat="dd/MM/yyyy"
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
+                dateFormat="dd/MM/yyyy"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
               />
             </div>
             <div className="inline space-b">
               <div className="card-title  mg-r-0-5">Arrivée</div>
               <DatePicker
-                  dateFormat="dd/MM/yyyy"
-                  selected={endDate}
-                  onChange={(date) => setEndDate(date)}
+                dateFormat="dd/MM/yyyy"
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
               />
             </div>
           </div>
@@ -138,50 +142,50 @@ export default function Quiz() {
             </div>
             <div className="button-group">
               <button
-                  id="btn1"
-                  onClick={handleClickBtn}
-                  className={
-                    btnActive === "btn1"
-                        ? "btn-active button btn-flex"
-                        : "button btn-flex"
-                  }
-                  type="button"
+                id="btn1"
+                onClick={handleClickBtn}
+                className={
+                  btnActive === "btn1"
+                    ? "btn-active button btn-flex"
+                    : "button btn-flex"
+                }
+                type="button"
               >
                 Culture
               </button>
               <button
-                  id="btn2"
-                  onClick={handleClickBtn}
-                  className={
-                    btnActive === "btn2"
-                        ? "btn-active button btn-flex"
-                        : "button btn-flex"
-                  }
-                  type="button"
+                id="btn2"
+                onClick={handleClickBtn}
+                className={
+                  btnActive === "btn2"
+                    ? "btn-active button btn-flex"
+                    : "button btn-flex"
+                }
+                type="button"
               >
                 Sport
               </button>
               <button
-                  id="btn3"
-                  onClick={handleClickBtn}
-                  className={
-                    btnActive === "btn3"
-                        ? "btn-active button btn-flex"
-                        : "button btn-flex"
-                  }
-                  type="button"
+                id="btn3"
+                onClick={handleClickBtn}
+                className={
+                  btnActive === "btn3"
+                    ? "btn-active button btn-flex"
+                    : "button btn-flex"
+                }
+                type="button"
               >
                 Nature
               </button>
               <button
-                  id="btn4"
-                  onClick={handleClickBtn}
-                  className={
-                    btnActive === "btn4"
-                        ? "btn-active button btn-flex"
-                        : "button btn-flex"
-                  }
-                  type="button"
+                id="btn4"
+                onClick={handleClickBtn}
+                className={
+                  btnActive === "btn4"
+                    ? "btn-active button btn-flex"
+                    : "button btn-flex"
+                }
+                type="button"
               >
                 Shopping
               </button>
@@ -191,10 +195,18 @@ export default function Quiz() {
           <div className="voyage-eco">
             ✅ <span className="t-bold">Voyage</span> écoresponsable
           </div>
-          <button type="button" className="button generate" onClick={ () => {handleSubmit(); handleDestination();}}>
-            Générer mon voyage →
-          </button>
-        </section>
-      </div>
+        </div>
+        <button
+          type="button"
+          className="button generate"
+          onClick={() => {
+            handleSubmit();
+            handleDestination();
+          }}
+        >
+          Générer mon voyage →
+        </button>
+      </section>
+    </div>
   );
 }
