506ba33e39550e29beaa4dc2d0af25568c1fc93d
Elie Parthenay <elie.parthenay@gmail.com>
2022-11-25 10:38:05 +0100

    update travel day toulouse

--- frontend/src/components/ResultItineraireToulouse.jsx
+++ frontend/src/components/ResultItineraireToulouse.jsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 // eslint-disable-next-line import/extensions
-import CardStep from "./CardStep.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+import CardStep from "./CardStep";
 import toulouseJour1 from "../assets/data/data.toulouseJour1.json";
+import Map from "./Map";
 
 import "./ResultItineraire.css";
 import toulouseJour2 from "../assets/data/data.toulouseJour2.json";
 
 function TripjourToulouse() {
+  const [showMap, setShowMap] = useState(0);
+
+  const changeMap = (day) => {
+    if (day === showMap) {
+      setShowMap(0);
+    } else {
+      setShowMap(day);
+    }
+  };
+
   return (
     <>
       <h1 className="title">
         <span className="t-bold">Découvrez</span> le programme{" "}
         <span className="t-bold">de votre voyage</span>
       </h1>
-      <map>CARTE</map>
+      <Map day={toulouseJour1.concat(toulouseJour2)} />
       <div className="jour">
-        <div className="explainsPart">
+        <div
+          role="presentation"
+          onKeyDown={() => changeMap(1)}
+          onClick={() => changeMap(1)}
+          className="explainsPart"
+        >
           <div className="headerDay">
+            <FontAwesomeIcon className="mapIcon" icon={faMapLocationDot} />
             <div className="dayCount">
               <div className="dayD">JOUR 1</div>
             </div>
             <div className="nameCity">CLERMONT-FERRAND</div>
           </div>
         </div>
+        {showMap === 1 ? <Map day={toulouseJour1} /> : false}
         {toulouseJour1.map((step) => (
           <CardStep
             key={step.id}
@@ -37,14 +57,21 @@ function TripjourToulouse() {
         ))}
       </div>
       <div className="jour">
-        <div className="explainsPart">
+        <div
+          role="presentation"
+          onKeyDown={() => changeMap(2)}
+          onClick={() => changeMap(2)}
+          className="explainsPart"
+        >
           <div className="headerDay">
+            <FontAwesomeIcon className="mapIcon" icon={faMapLocationDot} />
             <div className="dayCount">
-              <div className="dayD">JOUR 2</div>
+              <div className="dayD">JOUR 1</div>
             </div>
             <div className="nameCity">TOULOUSE</div>
           </div>
         </div>
+        {showMap === 2 ? <Map day={toulouseJour2} /> : false}
         {toulouseJour2.map((step) => (
           <CardStep
             key={step.id}
